fcc64bb9a222c5e917bde7972fe5a63a371b0038
Krzysiek <kr1sss@wp.pl>
2017-08-22 00:06:45 +0200

    changes changes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,24 @@
 AOS.init();
 
 $('#scroll-top').click(function() {
-	$('html, body').animate({ scrollTop:0 }, 1500);
+	$('html, body').animate({scrollTop:0 
+	}, 1500);
+});
+
+$('#About1').click(function() {
+	$('html, body').animate({
+		scrollTop: $('#About2').offset().top},
+		1500);
+});
+
+$('#Project1').click(function() {
+	$('html, body').animate({
+		scrollTop: $('#Project2').offset().top},
+		2000);
+});
+
+$('#Contact1').click(function() {
+	$('html, body').animate({
+		scrollTop: $('#Contact2').offset().top},
+		2500);
 });
