c05620049f514f1ffac74b61590afd194ae3f55b
Krzysiek <kr1sss@wp.pl>
2017-08-23 17:14:20 +0200

    change in transition

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,24 +1,26 @@
-AOS.init();
+AOS.init({
+	disable: window.innerWidth < 1024
+});
 
 $('#scroll-top').click(function() {
 	$('html, body').animate({scrollTop:0 
-	}, 1500);
+	}, 1000);
 });
 
 $('#About1').click(function() {
 	$('html, body').animate({
 		scrollTop: $('#About2').offset().top},
-		1500);
+		700);
 });
 
 $('#Project1').click(function() {
 	$('html, body').animate({
 		scrollTop: $('#Project2').offset().top},
-		2000);
+		1000);
 });
 
 $('#Contact1').click(function() {
 	$('html, body').animate({
 		scrollTop: $('#Contact2').offset().top},
-		2500);
+		1200);
 });
